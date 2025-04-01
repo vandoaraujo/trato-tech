0b85fcb8926ec0294addb9f35a96161bab0d3cbe
Vando <vando.araujo@serpro.gov.br>
2025-04-01 11:31:10 -0300

    alteração footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -19,7 +19,7 @@ export default function Footer() {
         <FaInstagram {...iconeProps} />
       </div>
       <span>
-        Desenvolvido por Alura.
+        Desenvolvido por Vando.
       </span>
     </footer>
   )
